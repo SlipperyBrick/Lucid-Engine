cad1900859ef6e5b58c7a3ac48b4843fbd4b6c7a
David James <dev.davejames@gmail.com>
2020-08-02 22:51:02 +0100

    Changed texture wrapping options to repeat to accomodate texture coordinates outside 0 - 1 range

--- Lucid/src/Lucid/Renderer/Texture.cpp
+++ Lucid/src/Lucid/Renderer/Texture.cpp
@@ -142,9 +142,9 @@ Texture2D::Texture2D(const std::string& path, bool srgb)
 
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
 
 			GLenum internalFormat = SetTextureFormat(instance->m_Format);
 
